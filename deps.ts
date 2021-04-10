85b6ca72f1395dafcaead313779e4bcd677186fe
Alisue <lambdalisue@hashnote.net>
2021-04-10 22:29:44 +0900

    Update denops to fix massive data issue

--- deps.ts
+++ deps.ts
@@ -1,5 +1,8 @@
 export type {
   Context,
   Dispatcher,
-} from "https://deno.land/x/denops@v0.9/mod.ts";
-export { Denops, getCacheOrElse } from "https://deno.land/x/denops@v0.9/mod.ts";
+} from "https://deno.land/x/denops@v0.10/mod.ts";
+export {
+  Denops,
+  getCacheOrElse,
+} from "https://deno.land/x/denops@v0.10/mod.ts";
