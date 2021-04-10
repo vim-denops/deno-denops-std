cfa1872aef321dcbb160ea9fafe11f1ea852cb3f
Alisue <lambdalisue@hashnote.net>
2021-04-10 21:06:57 +0900

    Update dependencies to fix massive data issue

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export type {
   Context,
   Dispatcher,
-} from "https://deno.land/x/denops@v0.7/mod.ts";
-export { Denops, getCacheOrElse } from "https://deno.land/x/denops@v0.7/mod.ts";
+} from "https://deno.land/x/denops@v0.9/mod.ts";
+export { Denops, getCacheOrElse } from "https://deno.land/x/denops@v0.9/mod.ts";
