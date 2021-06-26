881b35d4402a661950a800231c13d93a42f28161
Alisue <lambdalisue@hashnote.net>
2021-06-26 20:46:10 +0900

    :muscle: Export everything from denops

--- deps.ts
+++ deps.ts
@@ -1,15 +1,4 @@
 export * as path from "https://deno.land/std@0.95.0/path/mod.ts";
 export * as hash from "https://deno.land/std@0.95.0/hash/mod.ts";
 export * as fs from "https://deno.land/std@0.95.0/fs/mod.ts";
-
-export type {
-  Context,
-  Dispatcher,
-} from "https://deno.land/x/denops_core@v0.16.0/mod.ts";
-export {
-  Denops,
-  ensureArray,
-  ensureNumber,
-  ensureRecord,
-  ensureString,
-} from "https://deno.land/x/denops_core@v0.16.0/mod.ts";
+export * from "https://deno.land/x/denops_core@v0.16.0/mod.ts";
