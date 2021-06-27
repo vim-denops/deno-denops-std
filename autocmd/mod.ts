3cf7d9cdb3049663be22c4bd77b99222c9d996d8
Alisue <lambdalisue@hashnote.net>
2021-06-27 19:56:28 +0900

    :+1: Rewrite `autocmd` module to provide new functional style

--- autocmd/mod.ts
+++ autocmd/mod.ts
@@ -1,1 +1,16 @@
-export * from "./helper.ts";
+export type { AutocmdEvent } from "./types.ts";
+
+export { define, emit, emitAll, list, remove } from "./common.ts";
+export type {
+  DefineOptions,
+  EmitOptions,
+  ListOptions,
+  RemoveOptions,
+} from "./common.ts";
+
+export type {
+  GroupDefineOptions,
+  GroupHelper,
+  GroupRemoveOptions,
+} from "./group.ts";
+export { group } from "./group.ts";
--- deps.ts
+++ deps.ts
@@ -2,3 +2,4 @@ export * as path from "https://deno.land/std@0.95.0/path/mod.ts";
 export * as hash from "https://deno.land/std@0.95.0/hash/mod.ts";
 export * as fs from "https://deno.land/std@0.95.0/fs/mod.ts";
 export * from "https://deno.land/x/denops_core@v0.16.0/mod.ts";
+export * from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
