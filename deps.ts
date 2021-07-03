005a16c6a9c1867ce8cbb3bcc815a5f8098d3d59
kkiyama117 <k.kiyama117@gmail.com>
2021-07-03 13:38:55 +0900

    :package: Update denops.vim to v1.0.0-alpha.4

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * as path from "https://deno.land/std@0.95.0/path/mod.ts";
 export * as hash from "https://deno.land/std@0.95.0/hash/mod.ts";
 export * as fs from "https://deno.land/std@0.95.0/fs/mod.ts";
-export * from "https://deno.land/x/denops_core@v1.0.0-alpha.0/mod.ts";
+export * from "https://deno.land/x/denops_core@v1.0.0-alpha.4/mod.ts";
 export * from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
--- deps_test.ts
+++ deps_test.ts
@@ -1,2 +1,2 @@
 export * from "https://deno.land/std@0.95.0/testing/asserts.ts";
-export * from "https://deno.land/x/denops_core@v1.0.0-alpha.0/test/mod.ts";
+export * from "https://deno.land/x/denops_core@v1.0.0-alpha.4/test/mod.ts";
