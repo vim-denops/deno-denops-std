52b2c17670dabd8889de5c52301f85c7ad30598f
Alisue <lambdalisue@hashnote.net>
2021-07-04 20:04:41 +0900

    :memo: Add document comments on `function` module

--- denops_std/function/buffer.ts
+++ denops_std/function/buffer.ts
@@ -1,5 +1,13 @@
 import { Denops } from "../vendor/https/deno.land/x/denops_core/mod.ts";
 
+/**
+ * Add a buffer to the buffer list with {name}.
+ * If a buffer for file {name} already exists, return that buffer
+ * number.  Otherwise return the buffer number of the newly
+ * created buffer.  When {name} is an empty string then a new
+ * buffer is always created.
+ * The buffer will not have' 'buflisted' set.
+ */
 export async function bufadd(
   denops: Denops,
   name: string,
@@ -8,6 +16,28 @@ export async function bufadd(
   return bufnr as number;
 }
 
+/**
+ * The result is a Number, which is |TRUE| if a buffer called
+ * {expr} exists.
+ * If the {expr} argument is a number, buffer numbers are used.
+ * Number zero is the alternate buffer for the current window.
+ *
+ * If the {expr} argument is a string it must match a buffer name
+ * exactly.  The name can be:
+ * - Relative to the current directory.
+ * - A full path.
+ * - The name of a buffer with 'buftype' set to "nofile".
+ * - A URL name.
+ * Unlisted buffers will be found.
+ * Note that help files are listed by their short name in the
+ * output of |:buffers|, but bufexists() requires using their
+ * long name to be able to find them.
+ * bufexists() may report a buffer exists, but to use the name
+ * with a |:buffer| command you may need to use |expand()|.  Esp
+ * for MS-Windows 8.3 names in the form "c:\DOCUME~1"
+ * Use "bufexists(0)" to test for the existence of an alternate
+ * file name.
+ */
 export async function bufexists(
   denops: Denops,
   name: string | number,
@@ -16,6 +46,11 @@ export async function bufexists(
   return !!result;
 }
 
+/**
+ * The result is a Number, which is |TRUE| if a buffer called
+ * {expr} exists and is listed (has the 'buflisted' option set).
+ * The {expr} argument is used like with |bufexists()|.
+ */
 export async function buflisted(
   denops: Denops,
   name: string | number,
@@ -24,6 +59,15 @@ export async function buflisted(
   return !!result;
 }
 
+/**
+ * Ensure the buffer {expr} is loaded.  When the buffer name
+ * refers to an existing file then the file is read.  Otherwise
+ * the buffer will be empty.  If the buffer was already loaded
+ * then there is no change.
+ * If there is an existing swap file for the file of the buffer,
+ * there will be no dialog, the buffer will be loaded anyway.
+ * The {expr} argument is used like with |bufexists()|.
+ */
 export async function bufload(
   denops: Denops,
   name: string | number,
@@ -31,6 +75,11 @@ export async function bufload(
   await denops.call("bufload", name);
 }
 
+/**
+ * The result is a Number, which is |TRUE| if a buffer called
+ * {expr} exists and is loaded (shown in a window or hidden).
+ * The {expr} argument is used like with |bufexists()|.
+ */
 export async function bufloaded(
   denops: Denops,
   name: string | number,
@@ -39,6 +88,35 @@ export async function bufloaded(
   return !!result;
 }
 
+/**
+ * The result is the name of a buffer, as it is displayed by the
+ * ":ls" command.
+ * If {expr} is omitted the current buffer is used.
+ * If {expr} is a Number, that buffer number's name is given.
+ * Number zero is the alternate buffer for the current window.
+ * If {expr} is a String, it is used as a |file-pattern| to match
+ * with the buffer names.  This is always done like 'magic' is
+ * set and 'cpoptions' is empty.  When there is more than one
+ * match an empty string is returned.
+ * "" or "%" can be used for the current buffer, "#" for the
+ * alternate buffer.
+ * A full match is preferred, otherwise a match at the start, end
+ * or middle of the buffer name is accepted.  If you only want a
+ * full match then put "^" at the start and "$" at the end of the
+ * pattern.
+ * Listed buffers are found first.  If there is a single match
+ * with a listed buffer, that one is returned.  Next unlisted
+ * buffers are searched for.
+ * If the {expr} is a String, but you want to use it as a buffer
+ * number, force it to be a Number by adding zero to it: >
+ *     :echo bufname("3" + 0)
+ * If the buffer doesn't exist, or doesn't have a name, an empty
+ * string is returned.
+ *     bufname("#")		alternate buffer name
+ *     bufname(3)		name of buffer 3
+ *     bufname("%")		name of current buffer
+ *     bufname("file2")	name of buffer where "file2" matches.
+ */
 export async function bufname(
   denops: Denops,
   name?: string | number,
@@ -46,6 +124,20 @@ export async function bufname(
   return await denops.call("bufname", name) as string;
 }
 
+/**
+ * The result is the number of a buffer, as it is displayed by
+ * the ":ls" command.  For the use of {expr}, see |bufname()|
+ * above.
+ * If the buffer doesn't exist, -1 is returned.  Or, if the
+ * {create} argument is present and not zero, a new, unlisted,
+ * buffer is created and its number is returned.
+ * bufnr("$") is the last buffer:
+ * 	:let last_buffer = bufnr("$")
+ * The result is a Number, which is the highest buffer number
+ * of existing buffers.  Note that not all buffers with a smaller
+ * number necessarily exist, because ":bwipeout" may have removed
+ * them.  Use bufexists() to test for the existence of a buffer.
+ */
 export async function bufnr(
   denops: Denops,
   name: string | number,
@@ -53,6 +145,16 @@ export async function bufnr(
   return await denops.call("bufnr", name) as number;
 }
 
+/**
+ * The result is a Number, which is the |window-ID| of the first
+ * window associated with buffer {expr}.  For the use of {expr},
+ * see |bufname()| above.  If buffer {expr} doesn't exist or
+ * there is no such window, -1 is returned.  Example: >
+ *
+ *     echo "A window containing buffer 1 is " . (bufwinid(1))
+ *
+ * Only deals with the current tab page.
+ */
 export async function bufwinid(
   denops: Denops,
   name: string | number,
@@ -60,6 +162,18 @@ export async function bufwinid(
   return await denops.call("bufwinid", name) as number;
 }
 
+/**
+ * The result is a Number, which is the number of the first
+ * window associated with buffer {expr}.  For the use of {expr},
+ * see |bufname()| above.  If buffer {expr} doesn't exist or
+ * there is no such window, -1 is returned.  Example: >
+ *
+ *     echo "A window containing buffer 1 is " . (bufwinnr(1))
+ *
+ * The number can be used with |CTRL-W_w| and ":wincmd w"
+ * |:wincmd|.
+ * Only deals with the current tab page.
+ */
 export async function bufwinnr(
   denops: Denops,
   name: string | number,
@@ -67,6 +181,30 @@ export async function bufwinnr(
   return await denops.call("bufwinnr", name) as number;
 }
 
+/**
+ * Return a |List| with the lines starting from {lnum} to {end}
+ * (inclusive) in the buffer {expr}.  If {end} is omitted, a
+ * |List| with only the line {lnum} is returned.
+ *
+ * For the use of {expr}, see |bufname()| above.
+ *
+ * For {lnum} and {end} "$" can be used for the last line of the
+ * buffer.  Otherwise a number must be used.
+ *
+ * When {lnum} is smaller than 1 or bigger than the number of
+ * lines in the buffer, an empty |List| is returned.
+ *
+ * When {end} is greater than the number of lines in the buffer,
+ * it is treated as {end} is set to the number of lines in the
+ * buffer.  When {end} is before {lnum} an empty |List| is
+ * returned.
+ *
+ * This function works only for loaded buffers.  For unloaded and
+ * non-existing buffers, an empty |List| is returned.
+ *
+ * Example: >
+ * 	:let lines = getbufline(bufnr("myfile"), 1, "$")
+ */
 export async function getbufline(
   denops: Denops,
   name: string | number,
@@ -76,6 +214,19 @@ export async function getbufline(
   return await denops.call("getbufline", name, lnum, end) as string[];
 }
 
+/**
+ * Set line {lnum} to {text} in buffer {expr}.  To insert
+ * lines use |append()|.
+ *
+ * For the use of {expr}, see |bufname()| above.
+ *
+ * {lnum} is used like with |setline()|.
+ * This works like |setline()| for the specified buffer.
+ * On success 0 is returned, on failure 1 is returned.
+ *
+ * If {expr} is not a valid buffer or {lnum} is not valid, an
+ * error message is given.
+ */
 export async function setbufline(
   denops: Denops,
   name: string | number,
--- denops_std/function/common.ts
+++ denops_std/function/common.ts
@@ -1,5 +1,90 @@
 import { Denops } from "../vendor/https/deno.land/x/denops_core/mod.ts";
 
+/**
+ * The result is a Number, which is |TRUE| if {expr} is
+ * defined, zero otherwise.
+ *
+ * For checking for a supported feature use |has()|.
+ * For checking if a file exists use |filereadable()|.
+ *
+ * The {expr} argument is a string, which contains one of these:
+ * 	&option-name	Vim option (only checks if it exists,
+ * 			not if it really works)
+ * 	+option-name	Vim option that works.
+ * 	$ENVNAME	environment variable (could also be
+ * 			done by comparing with an empty
+ * 			string)
+ * 	*funcname	built-in function (see |functions|)
+ * 			or user defined function (see
+ * 			|user-functions|). Also works for a
+ * 			variable that is a Funcref.
+ * 	varname		internal variable (see
+ * 			|internal-variables|).  Also works
+ * 			for |curly-braces-names|, |Dictionary|
+ * 			entries, |List| items, etc.  Beware
+ * 			that evaluating an index may cause an
+ * 			error message for an invalid
+ * 			expression.  E.g.:
+ * 			   :let l = [1, 2, 3]
+ * 			   :echo exists("l[5]")
+ * 			   0
+ * 			   :echo exists("l[xx]")
+ * 			   E121: Undefined variable: xx
+ * 			   0
+ * 	:cmdname	Ex command: built-in command, user
+ * 			command or command modifier |:command|.
+ * 			Returns:
+ * 			1  for match with start of a command
+ * 			2  full match with a command
+ * 			3  matches several user commands
+ * 			To check for a supported command
+ * 			always check the return value to be 2.
+ * 	:2match		The |:2match| command.
+ * 	:3match		The |:3match| command.
+ * 	#event		autocommand defined for this event
+ * 	#event#pattern	autocommand defined for this event and
+ * 			pattern (the pattern is taken
+ * 			literally and compared to the
+ * 			autocommand patterns character by
+ * 			character)
+ * 	#group		autocommand group exists
+ * 	#group#event	autocommand defined for this group and
+ * 			event.
+ * 	#group#event#pattern
+ * 			autocommand defined for this group,
+ * 			event and pattern.
+ * 	##event		autocommand for this event is
+ * 			supported.
+ *
+ * Examples:
+ * 	exists("&mouse")
+ * 	exists("$HOSTNAME")
+ * 	exists("*strftime")
+ * 	exists("*s:MyFunc")
+ * 	exists("bufcount")
+ * 	exists(":Make")
+ * 	exists("#CursorHold")
+ * 	exists("#BufReadPre#*.gz")
+ * 	exists("#filetypeindent")
+ * 	exists("#filetypeindent#FileType")
+ * 	exists("#filetypeindent#FileType#*")
+ * 	exists("##ColorScheme")
+ * There must be no space between the symbol (&/$/* /#) and the
+ * name.
+ * There must be no extra characters after the name, although in
+ * a few cases this is ignored.  That may become more strict in
+ * the future, thus don't count on it!
+ * Working example:
+ * 	exists(":make")
+ * NOT working example:
+ * 	exists(":make install")
+
+ * Note that the argument must be a string, not the name of the
+ * variable itself.  For example:
+ * 	exists(bufcount)
+ * This doesn't check for existence of the "bufcount" variable,
+ * but gets the value of "bufcount", and checks if that exists.
+ */
 export async function exists(
   denops: Denops,
   name: string,
@@ -8,6 +93,49 @@ export async function exists(
   return !!result;
 }
 
+/**
+ * Returns 1 if {feature} is supported, 0 otherwise.  The
+ * {feature} argument is a feature name like "nvim-0.2.1" or
+ * "win32", see below.  See also |exists()|.
+ *
+ * Vim's compile-time feature-names (prefixed with "+") are not
+ * recognized because Nvim is always compiled with all possible
+ * features. |feature-compile|
+ *
+ * Feature names can be:
+ * 1.  Nvim version. For example the "nvim-0.2.1" feature means
+ *     that Nvim is version 0.2.1 or later:
+ * 	:if has("nvim-0.2.1")
+ *
+ * 2.  Runtime condition or other pseudo-feature. For example the
+ *     "win32" feature checks if the current system is Windows:
+ * 	:if has("win32")
+ * 					*feature-list*
+ *     List of supported pseudo-feature names:
+ *         acl		|ACL| support
+ *         iconv		Can use |iconv()| for conversion.
+ *         +shellslash	Can use backslashes in filenames (Windows)
+ * 	clipboard	|clipboard| provider is available.
+ * 	nvim		This is Nvim.
+ * 	python2		Legacy Vim |python2| interface. |has-python|
+ * 	python3		Legacy Vim |python3| interface. |has-python|
+ * 	pythonx		Legacy Vim |python_x| interface. |has-pythonx|
+ * 	ttyin		input is a terminal (tty)
+ * 	ttyout		output is a terminal (tty)
+ * 	unix		Unix system.
+ * 	*vim_starting*	True during |startup|.
+ * 	win32		Windows system (32 or 64 bit).
+ * 	wsl		WSL (Windows Subsystem for Linux) system
+ *
+ * 					*has-patch*
+ * 3.  Vim patch. For example the "patch123" feature means that
+ *     Vim patch 123 at the current |v:version| was included:
+ * 	:if v:version > 602 || v:version == 602 && has("patch148")
+ *
+ * 4.  Vim version. For example the "patch-7.4.237" feature means
+ *     that Nvim is Vim-compatible to version 7.4.237 or later.
+ * 	:if has("patch-7.4.237")
+ */
 export async function has(
   denops: Denops,
   name: string,
@@ -21,6 +149,30 @@ export async function has(
   return !!result;
 }
 
+/**
+ * Without {end} the result is a String, which is line {lnum}
+ * from the current buffer.  Example: >
+ * 	getline(1)
+ * When {lnum} is a String that doesn't start with a
+ * digit, line() is called to translate the String into a Number.
+ * To get the line under the cursor: >
+ * 	getline(".")
+ * When {lnum} is smaller than 1 or bigger than the number of
+ * lines in the buffer, an empty string is returned.
+ *
+ * When {end} is given the result is a |List| where each item is
+ * a line from the current buffer in the range {lnum} to {end},
+ * including line {end}.
+ * {end} is used in the same way as {lnum}.
+ * Non-existing lines are silently omitted.
+ * When {end} is before {lnum} an empty |List| is returned.
+ * Example: >
+ * 	:let start = line('.')
+ * 	:let end = search("^$") - 1
+ * 	:let lines = getline(start, end)
+ *
+ * To get lines from another buffer see |getbufline()|
+ */
 export async function getline(
   denops: Denops,
   lnum: string | number,
@@ -41,6 +193,31 @@ export async function getline(
   return await denops.call("getline", lnum) as string;
 }
 
+/**
+ * Set line {lnum} of the current buffer to {text}.  To insert
+ * lines use |append()|. To set lines in another buffer use
+ * |setbufline()|.
+ *
+ * {lnum} is used like with |getline()|.
+ * When {lnum} is just below the last line the {text} will be
+ * added as a new line.
+ *
+ * If this succeeds, 0 is returned.  If this fails (most likely
+ * because {lnum} is invalid) 1 is returned.
+ *
+ * Example:
+ * 	:call setline(5, strftime("%c"))
+ *
+ * When {text} is a |List| then line {lnum} and following lines
+ * will be set to the items in the list.  Example:
+ * 	:call setline(5, ['aaa', 'bbb', 'ccc'])
+ * This is equivalent to:
+ * 	:for [n, l] in [[5, 'aaa'], [6, 'bbb'], [7, 'ccc']]
+ * 	:  call setline(n, l)
+ * 	:endfor
+ *
+ * Note: The '[ and '] marks are not set.
+ */
 export async function setline(
   denops: Denops,
   lnum: string | number,
