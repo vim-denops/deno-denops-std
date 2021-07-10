6b6ef6eb4171ac6bc6d9878412934ad6a1353a56
Alisue <lambdalisue@hashnote.net>
2021-07-11 02:28:13 +0900

    :package: Update denops_core to v1.0.0-beta.3

--- denops_std/deps.ts
+++ denops_std/deps.ts
@@ -2,4 +2,4 @@ export * as fs from "https://deno.land/std@0.100.0/fs/mod.ts#^";
 export * as hash from "https://deno.land/std@0.100.0/hash/mod.ts#^";
 export * as path from "https://deno.land/std@0.100.0/path/mod.ts#^";
 
-export * from "https://deno.land/x/denops_core@v1.0.0-beta.1/mod.ts#^";
+export * from "https://deno.land/x/denops_core@v1.0.0-beta.3/mod.ts#^";
--- denops_std/deps_test.ts
+++ denops_std/deps_test.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/std@0.100.0/testing/asserts.ts#^";
 
-export * from "https://deno.land/x/denops_core@v1.0.0-beta.1/test/mod.ts#^";
+export * from "https://deno.land/x/denops_core@v1.0.0-beta.3/test/mod.ts#^";
