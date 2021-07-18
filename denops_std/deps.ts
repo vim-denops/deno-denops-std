4dca2e5555ae8dadfae2468d4403c9666630ab01
Alisue <lambdalisue@hashnote.net>
2021-07-19 00:14:55 +0900

    :package: Update deps

--- denops_std/deps.ts
+++ denops_std/deps.ts
@@ -1,8 +1,8 @@
-export * as fs from "https://deno.land/std@0.100.0/fs/mod.ts#^";
-export * as hash from "https://deno.land/std@0.100.0/hash/mod.ts#^";
-export * as path from "https://deno.land/std@0.100.0/path/mod.ts#^";
-export { deferred } from "https://deno.land/std@0.100.0/async/mod.ts#^";
+export * as fs from "https://deno.land/std@0.101.0/fs/mod.ts#^";
+export * as hash from "https://deno.land/std@0.101.0/hash/mod.ts#^";
+export * as path from "https://deno.land/std@0.101.0/path/mod.ts#^";
+export { deferred } from "https://deno.land/std@0.101.0/async/mod.ts#^";
 
-export * from "https://deno.land/x/denops_core@v1.0.0-beta.3/mod.ts#^";
+export * from "https://deno.land/x/denops_core@v1.0.0/mod.ts#^";
 
 export * from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
--- denops_std/deps_test.ts
+++ denops_std/deps_test.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.100.0/testing/asserts.ts#^";
+export * from "https://deno.land/std@0.101.0/testing/asserts.ts#^";
 
-export * from "https://deno.land/x/denops_core@v1.0.0-beta.7/test/mod.ts#^";
+export * from "https://deno.land/x/denops_core@v1.0.0/test/mod.ts#^";
