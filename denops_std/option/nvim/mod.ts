ca75d2dffe3a3145e566d3ce4336a6d3d1cb12a1
Alisue <lambdalisue@hashnote.net>
2021-08-13 13:57:40 +0900

    :+1: Add `option` module to access Vim/Neovim options

---
+++ denops_std/option/nvim/mod.ts
@@ -0,0 +1,5 @@
+// NOTE:
+// Do NOT add modules manually to this file.
+// Add modules to `_manual.ts` instead for manually written modules.
+export * from "./_manual.ts";
+export * from "./_generated.ts";
