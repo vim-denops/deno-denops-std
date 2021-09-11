93ec750d3625238bb67b75e5fbdcd9292efc8876
Alisue <lambdalisue@hashnote.net>
2021-09-11 13:53:12 +0900

    :package: Update dependencies

--- denops_std/deps.ts
+++ denops_std/deps.ts
@@ -3,6 +3,6 @@ export * as hash from "https://deno.land/std@0.106.0/hash/mod.ts#^";
 export * as path from "https://deno.land/std@0.106.0/path/mod.ts#^";
 export { deferred } from "https://deno.land/std@0.106.0/async/mod.ts#^";
 
-export * from "https://deno.land/x/denops_core@v1.9.0/mod.ts#^";
+export * from "https://deno.land/x/denops_core@v1.9.1/mod.ts#^";
 
 export * from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
