2f2c9b282a729e66cf65f1a9b9591e38082168bc
Alisue <lambdalisue@hashnote.net>
2021-09-12 14:32:58 +0900

    :herb: Add tests of `bufname` module to confirm example in README.md

--- denops_std/bufname/bufname_test.ts
+++ denops_std/bufname/bufname_test.ts
@@ -149,6 +149,49 @@ Deno.test("format encodes '#' in 'params'", () => {
   const exp = "denops:///absolute/path/to/worktree;foo=%23foo";
   assertEquals(dst, exp);
 });
+Deno.test("format pass example in README.md", () => {
+  assertEquals(
+    format({
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+    }),
+    "denops:///Users/John Titor/test.git",
+  );
+
+  assertEquals(
+    format({
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+      params: {
+        foo: "foo",
+        bar: ["bar1", "bar2"],
+      },
+    }),
+    "denops:///Users/John Titor/test.git;foo=foo&bar=bar1&bar=bar2",
+  );
+
+  assertEquals(
+    format({
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+      fragment: "README.md",
+    }),
+    "denops:///Users/John Titor/test.git#README.md",
+  );
+
+  assertEquals(
+    format({
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+      params: {
+        foo: "foo",
+        bar: ["bar1", "bar2"],
+      },
+      fragment: "README.md",
+    }),
+    "denops:///Users/John Titor/test.git;foo=foo&bar=bar1&bar=bar2#README.md",
+  );
+});
 
 Deno.test("parse throws exception when 'expr' contains unusable characters", () => {
   const src = "denops:///<>|?*";
@@ -285,4 +328,49 @@ Deno.test("parse decode percent-encoded characters ('#') in 'params'", () => {
     },
   };
   assertEquals(dst, exp);
+});
+Deno.test("parse pass example in README.md", () => {
+  assertEquals(
+    parse("denops:///Users/John Titor/test.git"),
+    {
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+    },
+  );
+
+  assertEquals(
+    parse("denops:///Users/John Titor/test.git;foo=foo&bar=bar1&bar=bar2"),
+    {
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+      params: {
+        foo: "foo",
+        bar: ["bar1", "bar2"],
+      },
+    },
+  );
+
+  assertEquals(
+    parse("denops:///Users/John Titor/test.git#README.md"),
+    {
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+      fragment: "README.md",
+    },
+  );
+
+  assertEquals(
+    parse(
+      "denops:///Users/John Titor/test.git;foo=foo&bar=bar1&bar=bar2#README.md",
+    ),
+    {
+      scheme: "denops",
+      expr: "/Users/John Titor/test.git",
+      params: {
+        foo: "foo",
+        bar: ["bar1", "bar2"],
+      },
+      fragment: "README.md",
+    },
+  );
 });
