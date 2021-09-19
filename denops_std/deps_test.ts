632235a7a6fe3dd555b54a6111565083e78ba254
Alisue <lambdalisue@hashnote.net>
2021-09-19 15:48:24 +0900

    :package: Downgrade and fix version of std module
    https://github.com/denoland/deno_std/releases/tag/0.107.0 contains a breaking change
    which drop support of Deno 1.11.0

--- denops_std/deps_test.ts
+++ denops_std/deps_test.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.107.0/testing/asserts.ts#^";
+export * from "https://deno.land/std@0.106.0/testing/asserts.ts#=";
 
 export * from "https://deno.land/x/denops_core@v1.10.1/test/mod.ts#^";
