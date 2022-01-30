40832222d75666356c6f2d85b6dfeff709072f2c
Alisue <lambdalisue@gmail.com>
2022-01-30 15:23:52 +0900

    :package: Update denops_core to v3.0.0

--- denops_std/deps.ts
+++ denops_std/deps.ts
@@ -3,6 +3,6 @@ export * as hash from "https://deno.land/std@0.123.0/hash/mod.ts";
 export * as path from "https://deno.land/std@0.123.0/path/mod.ts";
 export { deferred } from "https://deno.land/std@0.123.0/async/mod.ts";
 
-export * from "https://deno.land/x/denops_core@v2.2.0/mod.ts#^";
+export * from "https://deno.land/x/denops_core@v3.0.0/mod.ts#^";
 
 export * from "https://deno.land/x/unknownutil@v1.1.4/mod.ts#^";
