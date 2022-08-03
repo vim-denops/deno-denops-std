356d16019c702ff4a36fac8828687060d5d4cb4d
Alisue <lambdalisue@gmail.com>
2022-08-04 04:18:40 +0900

    :+1: Expose `FileFormat` and related functions

--- denops_std/buffer/mod.ts
+++ denops_std/buffer/mod.ts
@@ -1,2 +1,9 @@
 export * from "./buffer.ts";
 export * from "./decoration.ts";
+export {
+  assertFileFormat,
+  ensureFileFormat,
+  isFileFormat,
+  maybeFileFormat,
+} from "./fileformat.ts";
+export type { FileFormat } from "./fileformat.ts";
