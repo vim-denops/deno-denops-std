e10dc36f372df32514bdfda9482c4fa402f2cc94
Alisue <lambdalisue@gmail.com>
2022-12-11 21:01:57 +0900

    :+1: Add a workaround for `getreginfo` in Vim
    Close #155

--- denops_std/function/_manual.ts
+++ denops_std/function/_manual.ts
@@ -1,6 +1,7 @@
 export * from "./buffer.ts";
 export * from "./common.ts";
 export * from "./cursor.ts";
+export * from "./getreginfo.ts";
 export * from "./input.ts";
 export * from "./types.ts";
 export * from "./various.ts";
