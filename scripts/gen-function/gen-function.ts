39eca8f8450fbda4563e484d7f2dd7ab983d4e94
Alisue <lambdalisue@gmail.com>
2023-01-01 04:14:01 +0900

    :boom: Follow the latest denops.vim support version
    https://github.com/vim-denops/denops.vim/pull/207

--- scripts/gen-function/gen-function.ts
+++ scripts/gen-function/gen-function.ts
@@ -10,8 +10,8 @@ import { parse } from "./parse.ts";
 import { format } from "./format.ts";
 import { downloadString } from "./utils.ts";
 
-const VIM_VERSION = "8.2.3452";
-const NVIM_VERSION = "0.6.0";
+const VIM_VERSION = "9.0.0472";
+const NVIM_VERSION = "0.8.0";
 
 const manualFnSet = new Set([
   ...Object.keys(commonManual),
