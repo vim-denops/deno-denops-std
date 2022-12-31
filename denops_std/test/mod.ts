0ff9f1a1454fd8e6af9c7d24cd22941f99fb1367
Alisue <lambdalisue@gmail.com>
2022-12-31 23:42:01 +0900

    :memo: Add deprecation message on `test` module

--- denops_std/test/mod.ts
+++ denops_std/test/mod.ts
@@ -1,3 +1,8 @@
-// Re-export
+/**
+ * This module is deprecated. Use https://deno.land/x/denops_test/mod.ts instead.
+ *
+ * @deprecated
+ * @module
+ */
 export { test } from "https://deno.land/x/denops_core@v3.3.0/test/mod.ts";
 export type { TestDefinition } from "https://deno.land/x/denops_core@v3.3.0/test/mod.ts";
