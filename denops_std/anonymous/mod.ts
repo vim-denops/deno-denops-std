5ecff185512f10a729c72340ccddab678145fab4
Alisue <lambdalisue@gmail.com>
2023-01-11 00:21:29 +0900

    :memo: Add documentation comments on `anonymous`

--- denops_std/anonymous/mod.ts
+++ denops_std/anonymous/mod.ts
@@ -1,3 +1,9 @@
+/**
+ * A module to provide anonymous function which is callable from
+ * outside of the plugin (Vim or other plugins.)
+ *
+ * @module
+ */
 import type { Denops } from "https://deno.land/x/denops_core@v3.4.1/mod.ts";
 
 // https://github.com/microsoft/TypeScript/issues/26223#issuecomment-674500430
@@ -19,7 +25,36 @@ export type Identifier = string;
 export type Callback = (...args: unknown[]) => Promise<unknown> | unknown;
 
 /**
- * Add anonymous functions as a denops API and return the identifiers
+ * Add anonymous functions as denops API and return the identifiers
+ *
+ * ```typescript
+ * import { Denops } from "../mod.ts";
+ * import * as anonymous from "./mod.ts";
+ *
+ * export async function main(denops: Denops): Promise<void> {
+ *   // Add anonymous functions
+ *   const ids = anonymous.add(
+ *     denops,
+ *     () => {
+ *       // Do what ever you want.
+ *     },
+ *     () => {
+ *       // Do what ever you want.
+ *     },
+ *     // You can add as many callbacks as you want...
+ *   );
+ *
+ *   // Use ids to dispatch added functions from Deno
+ *   await denops.dispatch(denops.name, ids[0]);
+ *   await denops.dispatch(denops.name, ids[1]);
+ *
+ *   // Or from Vim
+ *   await denops.cmd("call denops#notify(name, id, [])", {
+ *     name: denops.name,
+ *     id: ids[1],
+ *   });
+ * }
+ * ```
  */
 export function add<N extends number>(
   denops: Denops,
@@ -36,7 +71,33 @@ export function add<N extends number>(
 }
 
 /**
- * Add oneshot anonymous functions as a denops API and return the identifiers
+ * Add oneshot anonymous functions as denops API and return the identifiers
+ *
+ * ```typescript
+ * import { Denops } from "../mod.ts";
+ * import * as anonymous from "./mod.ts";
+ *
+ * export async function main(denops: Denops): Promise<void> {
+ *   // Add anonymous functions
+ *   const ids = anonymous.once(
+ *     denops,
+ *     () => {
+ *       // Do what ever you want.
+ *     },
+ *     () => {
+ *       // Do what ever you want.
+ *     },
+ *     // You can add as many callbacks as you want...
+ *   );
+ *
+ *   // First calls complete normally
+ *   await denops.dispatch(denops.name, ids[0]);
+ *   await denops.dispatch(denops.name, ids[1]);
+ *
+ *   // But second call would throw errors
+ *   await denops.dispatch(denops.name, ids[0]);
+ * }
+ * ```
  */
 export function once<N extends number>(
   denops: Denops,
@@ -57,7 +118,35 @@ export function once<N extends number>(
 }
 
 /**
- * Remove anonymous functions identified by names from a denops API
+ * Remove anonymous functions from denops API identified by the identifiers
+ *
+ * ```typescript
+ * import { Denops } from "../mod.ts";
+ * import * as anonymous from "./mod.ts";
+ *
+ * export async function main(denops: Denops): Promise<void> {
+ *   // Add anonymous functions
+ *   const ids = anonymous.add(
+ *     denops,
+ *     () => {
+ *       // Do what ever you want.
+ *     },
+ *     () => {
+ *       // Do what ever you want.
+ *     },
+ *     // You can add as many callbacks as you want...
+ *   );
+ *
+ *   // Remove ids[1]
+ *   anonymous.remove(denops, ids[1]);
+ *
+ *   // Call of ids[0] complete normally
+ *   await denops.dispatch(denops.name, ids[0]);
+ *
+ *   // But ids[1] is already removed
+ *   await denops.dispatch(denops.name, ids[1]);
+ * }
+ * ```
  */
 export function remove<N extends number>(
   denops: Denops,
