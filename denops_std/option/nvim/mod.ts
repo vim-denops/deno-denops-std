17c3c2e7770c6d6c87efc5d403e1d3620001fb06
Alisue <lambdalisue@gmail.com>
2023-01-11 01:16:41 +0900

    :memo: Add documentation comments on `options`

--- denops_std/option/nvim/mod.ts
+++ denops_std/option/nvim/mod.ts
@@ -1,3 +1,18 @@
+/**
+ * A module to provide accessors of Neovim native options.
+ *
+ * ```typescript
+ * import { Denops } from "../../mod.ts";
+ * import * as nvimOp from "../../option/nvim/mod.ts";
+ *
+ * export async function main(denops: Denops): Promise<void> {
+ *   // nvimOp has options only exist in Neovim
+ *   console.log(await nvimOp.shada.get(denops));
+ * }
+ * ```
+ *
+ * @module
+ */
 // NOTE:
 // Do NOT add modules manually to this file.
 // Add modules to `_manual.ts` instead for manually written modules.
