a470596e454a24cf5730a0a74668b7325a960af5
Milly <milly.ca@gmail.com>
2023-04-11 08:37:50 +0900

    :coffee: Fix options parse

--- scripts/gen-option/types.ts
+++ scripts/gen-option/types.ts
@@ -1,6 +1,22 @@
 export type Option = {
   name: string;
-  type: string;
-  scope: string[];
+  type: OptionType;
+  scope: OptionScope[];
   docs: string;
 };
+
+export const OPTION_TYPES = ["string", "number", "boolean"] as const;
+
+export type OptionType = typeof OPTION_TYPES[number];
+
+export function isOptionType(x: unknown): x is OptionType {
+  return OPTION_TYPES.includes(x as OptionType);
+}
+
+export const OPTION_SCOPES = ["global", "local"] as const;
+
+export type OptionScope = typeof OPTION_SCOPES[number];
+
+export function isOptionScope(x: unknown): x is OptionScope {
+  return OPTION_SCOPES.includes(x as OptionScope);
+}
