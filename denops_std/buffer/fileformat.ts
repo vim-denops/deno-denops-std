60e334a9e8ffd515e66acc8925dd14d0730c7a53
Alisue <lambdalisue@gmail.com>
2024-01-06 18:10:49 +0900

    :package: bump deno.land/x/unknownutil from v3.10.0 to v3.11.0

--- denops_std/buffer/fileformat.ts
+++ denops_std/buffer/fileformat.ts
@@ -4,7 +4,7 @@ import {
   is,
   maybe,
   PredicateType,
-} from "https://deno.land/x/unknownutil@v3.10.0/mod.ts#^";
+} from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
 
 /**
  * Predicate that the value is FileFormat.
