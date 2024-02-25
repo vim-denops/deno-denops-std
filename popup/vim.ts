fa4c32a3e5eecdb87d95e86e20d1de4b1bef7692
Alisue <lambdalisue@gmail.com>
2024-02-25 15:22:28 +0900

    :+1: Add `popup` module as a compatibility layer for popup window in Vim and Neovim.

---
+++ popup/vim.ts
@@ -0,0 +1,101 @@
+import type { Denops } from "../mod.ts";
+import * as vimFn from "../function/vim/mod.ts";
+import { unreachable } from "https://deno.land/x/errorutil@v0.1.1/mod.ts";
+
+import type { Anchor, Border, OpenOptions } from "./types.ts";
+
+export function openPopup(
+  denops: Denops,
+  bufnr: number,
+  options: Omit<OpenOptions, "bufnr" | "noRedraw">,
+): Promise<number> {
+  const popupCreateOptions = toPopupCreateOptions(options);
+  return vimFn.popup_create(denops, bufnr, popupCreateOptions) as Promise<
+    number
+  >;
+}
+
+export async function closePopup(denops: Denops, winid: number): Promise<void> {
+  await vimFn.popup_close(denops, winid);
+}
+
+function toPopupCreateOptions(options: OpenOptions): vimFn.PopupCreateOptions {
+  const v: vimFn.PopupCreateOptions = {
+    line: options.row,
+    col: options.col,
+    pos: options.anchor ? posFromAnchor(options.anchor) : undefined,
+    posinvert: false, // To keep consistent with the behavior of Neovim's floating window
+    fixed: true, // To keep consistent with the behavior of Neovim's floating window
+    flip: false, // To keep consistent with the behavior of Neovim's floating window
+    maxheight: options.height,
+    minheight: options.height,
+    maxwidth: options.width,
+    minwidth: options.width,
+    title: options.title,
+    highlight: options.highlight?.normal,
+    borderhighlight: options.highlight?.border
+      ? [options.highlight?.border]
+      : undefined,
+    scrollbar: 0, // To keep consistent with the behavior of Neovim's floating window
+    zindex: options.zindex,
+  };
+  if (options.border) {
+    const { border, borderchars } = toVimBorder(options.border);
+    v.border = border;
+    v.borderchars = borderchars;
+  }
+  return Object.fromEntries(
+    Object
+      .entries(v)
+      .filter(([, v]) => v != undefined),
+  ) as vimFn.PopupCreateOptions;
+}
+
+function posFromAnchor(anchor: Anchor): vimFn.PopupCreateOptions["pos"] {
+  switch (anchor) {
+    case "NW":
+      return "topleft";
+    case "NE":
+      return "topright";
+    case "SW":
+      return "botleft";
+    case "SE":
+      return "botright";
+    default:
+      unreachable(anchor);
+  }
+}
+
+function toVimBorder(
+  border: Border,
+): {
+  border: vimFn.PopupCreateOptions["border"];
+  borderchars: vimFn.PopupCreateOptions["borderchars"];
+} {
+  if (typeof border === "string") {
+    switch (border) {
+      case "single":
+        return {
+          border: [],
+          borderchars: ["─", "│", "─", "│", "┌", "┐", "┘", "└"],
+        };
+      case "double":
+        return {
+          border: [],
+          borderchars: ["═", "║", "═", "║", "╔", "╗", "╝", "╚"],
+        };
+      case "rounded":
+        return {
+          border: [],
+          borderchars: ["─", "│", "─", "│", "╭", "╮", "╯", "╰"],
+        };
+      default:
+        unreachable(border);
+    }
+  }
+  const [lt, t, rt, r, rb, b, lb, l] = border;
+  return {
+    border: [t ? 1 : 0, r ? 1 : 0, b ? 1 : 0, l ? 1 : 0],
+    borderchars: [t, r, b, l, lt, rt, rb, lb],
+  };
+}
