46417cf715761abecd6c56fbcf0a07f2222c5d62
Alisue <lambdalisue@gmail.com>
2024-05-13 03:39:44 +0900

    :bug: Fix `mode` test
    Because of https://github.com/vim-denops/deno-denops-test/pull/15, now
    tests is running with normal mode even on Vim.

--- function/various_test.ts
+++ function/various_test.ts
@@ -6,8 +6,8 @@ test({
   mode: "all",
   name: "mode()",
   fn: async (denops) => {
-    const shortMode = denops.meta.host === "vim" ? "c" : "n";
-    const longMode = denops.meta.host === "vim" ? "ce" : "n";
+    const shortMode = "n";
+    const longMode = "n";
     assertEquals(await various.mode(denops), shortMode);
     assertEquals(await various.mode(denops, 0), shortMode);
     assertEquals(await various.mode(denops, 1), longMode);
