1222ee57ba91f66e5ecd276dd89b597732fa280b
Milly <milly.ca@gmail.com>
2024-08-14 21:52:28 +0900

    :herb: improve tests for helper/keymap

--- helper/keymap_test.ts
+++ helper/keymap_test.ts
@@ -1,57 +1,70 @@
 import { assertEquals } from "@std/assert";
-import { is } from "@core/unknownutil";
 import { test } from "@denops/test";
+import { AsyncDisposableStack } from "@nick/dispose";
 import * as fn from "../function/mod.ts";
-import { exprQuote as q } from "./expr_string.ts";
-import { type KeysSpecifier, send } from "./keymap.ts";
-
-function toArray<T>(x: T | T[]): T[] {
-  return is.Array(x) ? x : [x];
-}
-
-type Spec = {
-  name: string;
-  keys: KeysSpecifier | KeysSpecifier[];
-  expect: string;
-};
+import { exprQuote } from "./expr_string.ts";
+import { send } from "./keymap.ts";
 
 test({
-  mode: "all",
+  mode: "vim",
   name: "send()",
   fn: async (denops, t) => {
-    const specs: Spec[] = [
-      {
-        name: "normal key",
-        keys: "foo",
-        expect: "foo",
-      },
-      {
-        name: "special key",
-        keys: q`foo\<BS>bar\<Left>\<Del>`,
-        expect: "foba",
-      },
-      {
-        name: "remapped key",
-        keys: { keys: q`\<C-l>`, remap: true },
-        expect: "foo",
-      },
-    ];
-    await denops.cmd("inoremap <C-l> foo");
-
-    for (const spec of specs) {
-      await t.step({
-        name: spec.name,
-        fn: async () => {
-          await fn.deletebufline(denops, "%", 1, "$");
-          assertEquals(await fn.getline(denops, 1), "");
-          await send(denops, [
-            q`\<Esc>`,
-            "i",
-            ...toArray(spec.keys),
-          ]);
-          assertEquals(await fn.getline(denops, 1), spec.expect);
-        },
-      });
-    }
+    await t.step("sends normal keys with string", async () => {
+      await fn.deletebufline(denops, "%", 1, "$");
+      await fn.setline(denops, 1, "foobar");
+      await send(denops, "gg0lll");
+      assertEquals(await fn.getpos(denops, "."), [0, 1, 4, 0]);
+    });
+    await t.step("sends normal keys with string[]", async () => {
+      await fn.deletebufline(denops, "%", 1, "$");
+      await fn.setline(denops, 1, "foobar");
+      await send(denops, ["gg0lll", "hh", "lll"]);
+      assertEquals(await fn.getpos(denops, "."), [0, 1, 5, 0]);
+    });
+    await t.step("sends special keys with ExprString", async () => {
+      await fn.deletebufline(denops, "%", 1, "$");
+      await send(denops, exprQuote`\<Cmd>call setline(1, 'foo')\<CR>`);
+      assertEquals(await fn.getline(denops, 1), "foo");
+    });
+    await t.step("sends special keys with ExprString[]", async () => {
+      await fn.deletebufline(denops, "%", 1, "$");
+      await send(denops, [
+        exprQuote`\<Cmd>call setline(1, 'foo')\<CR>`,
+        exprQuote`\<Cmd>call append(0, 'bar')\<CR>`,
+        exprQuote`\<Cmd>call append(0, 'baz')\<CR>`,
+      ]);
+      assertEquals(await fn.getline(denops, 1, "$"), ["baz", "bar", "foo"]);
+    });
+    await t.step("sends not remapped keys with Keys", async () => {
+      await using stack = new AsyncDisposableStack();
+      stack.defer(() => denops.cmd("nunmap k"));
+      await denops.cmd("nnoremap k <Down>");
+      await fn.deletebufline(denops, "%", 1, "$");
+      await fn.setline(denops, 1, ["foo", "bar", "baz"]);
+      await send(denops, { keys: "gg0jlk", remap: false });
+      assertEquals(await fn.getpos(denops, "."), [0, 1, 2, 0]);
+    });
+    await t.step("sends remapped keys with Keys", async () => {
+      await using stack = new AsyncDisposableStack();
+      stack.defer(() => denops.cmd("nunmap k"));
+      await denops.cmd("nnoremap k <Down>");
+      await fn.deletebufline(denops, "%", 1, "$");
+      await fn.setline(denops, 1, ["foo", "bar", "baz"]);
+      await send(denops, { keys: "gg0jlk", remap: true });
+      assertEquals(await fn.getpos(denops, "."), [0, 3, 2, 0]);
+    });
+    await t.step("sends mixed keys with Keys[]", async () => {
+      await using stack = new AsyncDisposableStack();
+      stack.defer(() => denops.cmd("nunmap k"));
+      await denops.cmd("nnoremap k <Down>");
+      await fn.deletebufline(denops, "%", 1, "$");
+      await fn.setline(denops, 1, ["foo", "bar", "baz"]);
+      await send(denops, [
+        { keys: exprQuote`gg0\<Down>lk`, remap: false },
+        { keys: "k", remap: true },
+        { keys: "k", remap: false },
+      ]);
+      assertEquals(await fn.getpos(denops, "."), [0, 1, 2, 0]);
+    });
   },
 });
