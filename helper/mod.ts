c1054151f81610a7e21fabd2cdd281e327a4c6e1
mityu <mityu.mail@gmail.com>
2024-12-25 23:58:13 +0900

    :+1: Add getbufinfo() helper function
    When a funcref appears in buffer-local variables, calling getbufinfo()
    from denops world will fail due to the error on serializing its return
    value using json_encode().
    To avoid this, we add a wrapper function of getbufinfo() to filter-out
    buffer-local variables from its return value.

--- helper/mod.ts
+++ helper/mod.ts
@@ -6,6 +6,7 @@
 export * from "./echo.ts";
 export * from "./execute.ts";
 export * from "./expr_string.ts";
+export * from "./getbufinfo.ts";
 export * from "./input.ts";
 export * from "./keymap.ts";
 export * from "./load.ts";
